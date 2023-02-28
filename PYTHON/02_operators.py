d8f31ef4937db81988044565ff11816a566a7d78
DevFelixJ <felixj.developer@gmail.com>
2023-02-28 12:00:38 +0100

    Actualizacion del script

--- PYTHON/02_operators.py
+++ PYTHON/02_operators.py
@@ -1,1 +1,19 @@
 ### Operadores ###
+
+print(3 + 5)
+print(3 - 5)
+print(3 * 5)
+print (3 / 5)
+print(3 % 5)
+print (3 // 5) #Cociente
+print (3 ** 5) #Exponente. 3 ELEVADO A 5
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+string_hola = "Hola "
+my_float = 2.5 * 2
+
+print(string_hola + "Python " + "Que te cuentas?")
+print (string_hola + "5 " + str(7))
+print(string_hola * 5)
+print("Con my_float " + string_hola * int(my_float))
+
